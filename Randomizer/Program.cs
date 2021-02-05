9af3d95dd1dd1004fd508d7cd10993b187550389
Michael Perez <mperez6473@gmail.com>
2021-02-04 20:49:47 -0600

    Add project files.

---
+++ Randomizer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+// Container namespace called Randomizer
+namespace Randomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
