e211bbab4bb2b8ca004f6397a4c02fd730ded1ec
Michael Perez <mperez6473@gmail.com>
2021-02-12 21:49:31 -0600

    First program in cpp, just a basic hello world style program

---
+++ cpp/myProgram.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "This is a basic program that just returns this text! Nothing much to see really.";
+  system("pause");
+  return 0;
+}
